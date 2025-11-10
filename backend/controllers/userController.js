a1f48096fa50c4ff64ae353d8fe94b0f2fa2a5b5
Ritika Sharma <ritikasharma29112003@gmail.com>
2025-11-10 22:57:11 +0530

    update: latest backend changes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+
+
+const loginUser=async(req,res)=>{
+
+}
+const registerUser=async(req,res)=>{
+res.json({msg:"Register API is Working"});
+}
+const adminLogin=async(req,res)=>{
+
+}
+export {loginUser,registerUser,adminLogin}
--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 const app=express();
-import connectDB from './config/mongodb';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
 const port=process.env.PORT||4000
 connectDB();
+connectCloudinary();
 app.use(express.json())
 app.use(cors())
+app.use('/api/user',userRouter)
 app.get('/',(req,res)=>{
     res.send('API WORKING');
 })
