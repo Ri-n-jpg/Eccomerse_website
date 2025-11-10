3d612b17fe2c849b31ab11d7010148959ad7276e
Ritika Sharma <ritikasharma29112003@gmail.com>
2025-11-10 20:02:42 +0530

    Daily update: <short note about changes>

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 const app=express();
+import connectDB from './config/mongodb';
 const port=process.env.PORT||4000
+connectDB();
 app.use(express.json())
 app.use(cors())
 app.get('/',(req,res)=>{
